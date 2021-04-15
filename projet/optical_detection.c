90d04b501bbc3ddd71b684f28adafbeecbcf3bf9
lara <lara.laamari@epfl.ch>
2021-04-15 11:50:14 +0200

    creation optical detection

---
+++ projet/optical_detection.c
@@ -0,0 +1,22 @@
+#include "ch.h"
+#include "hal.h"
+#include <main.h>
+#include <usbcfg.h>
+#include <chprintf.h>
+#include <optical_detection.h>
+
+
+
+
+
+
+/*
+ * optical_detection.c
+ *
+ *  Created on: 15 avr. 2021
+ *      Author: laral
+ */
+
+
+
+
---
+++ projet/optical_detection.h
@@ -0,0 +1,15 @@
+/*
+ * optical_detection.h
+ *
+ *  Created on: 15 avr. 2021
+ *      Author: laral
+ */
+
+#ifndef MINI_PROJET_ROBOTIQUE_PROJET_OPTICAL_DETECTION_H_
+#define MINI_PROJET_ROBOTIQUE_PROJET_OPTICAL_DETECTION_H_
+
+
+
+
+
+#endif /* MINI_PROJET_ROBOTIQUE_PROJET_OPTICAL_DETECTION_H_ */
