9a039a1d14cf74fe801edca72ffa0eb6f0c7c0a3
Antoine Perrin <antoinepierreperrin@gmail.com>
2021-04-15 20:06:15 +0200

    Use of TP5's code build success

--- projet/fft.h
+++ projet/fft.h
@@ -10,6 +10,16 @@
 
 
 
+typedef struct complex_float{
+	float real;
+	float imag;
+}complex_float;
+
+void doFFT_optimized(uint16_t size, float* complex_buffer);
+
+void doFFT_c(uint16_t size, complex_float* complex_buffer);
+
+
 
 
 #endif /* PROJET_FFT_H_ */
--- projet/main.c
+++ projet/main.c
@@ -10,18 +10,70 @@
 #include <main.h>
 
 
+#include <usbcfg.h>
+#include <chprintf.h>
+#include <audio/microphone.h>
+
+#include <audio_processing.h>
+#include <fft.h>
+#include <communications.h>
+#include <arm_math.h>
+
+
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
+
+static void timer12_start(void){
+    //General Purpose Timer configuration
+    //timer 12 is a 16 bit timer so we can measure time
+    //to about 65ms with a 1Mhz counter
+    static const GPTConfig gpt12cfg = {
+        1000000,        /* 1MHz timer clock in order to measure uS.*/
+        NULL,           /* Timer callback.*/
+        0,
+        0
+    };
+
+    gptStart(&GPTD12, &gpt12cfg);
+    //let the timer count to max value
+    gptStartContinuous(&GPTD12, 0xFFFF);
+}
+
 int main(void)
 {
 
-    halInit();
-    chSysInit();
-    mpu_init();
+	 halInit();
+	 chSysInit();
+	 mpu_init();
+
+	 //starts the serial communication / the USB communication / timer 12 / inits the motors
+	 serial_start();
+	 usb_start();
+	 timer12_start();
+	 motors_init();
+
+
+	 //starts the microphones processing thread.
+	 //it calls the callback given in parameter when samples are ready
+	 mic_start(&processAudioData);
 
 
     /* Infinite loop. */
     while (1) {
     	//waits 1 second
         chThdSleepMilliseconds(1000);
+
+        //waits until a result must be sent to the computer
+        wait_send_to_computer();
     }
 }
 
