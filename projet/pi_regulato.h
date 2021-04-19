f2bdbf58ed5cbc034c895583291bcdfc872b61b0
lara <lara.laamari@epfl.ch>
2021-04-20 01:18:08 +0200

    fichier chconf,halconf etc

---
+++ projet/pi_regulato.h
@@ -0,0 +1,17 @@
+/*
+ * pi_regulato.h
+ *
+ *  Created on: 20 avr. 2021
+ *      Author: laral
+ */
+
+#ifndef MINI_PROJET_ROBOTIQUE_PROJET_PI_REGULATO_H_
+#define MINI_PROJET_ROBOTIQUE_PROJET_PI_REGULATO_H_
+
+
+//start the PI regulator thread
+void pi_regulator_start(void);
+
+
+
+#endif /* MINI_PROJET_ROBOTIQUE_PROJET_PI_REGULATO_H_ */
