b02d29154cc7c3b0eaf3cd92d38bc87039f8696f
lara <lara.laamari@epfl.ch>
2021-05-12 14:48:11 +0200

    comit avant code clean

--- projet/animation.h
+++ projet/animation.h
@@ -1,10 +1,3 @@
-/*
- * animation.h
- *
- *  Created on: 5 mai 2021
- *      Author: laral
- */
-
 #ifndef MINI_PROJET_ROBOTIQUE_PROJET_ANIMATION_H_
 #define MINI_PROJET_ROBOTIQUE_PROJET_ANIMATION_H_
 
